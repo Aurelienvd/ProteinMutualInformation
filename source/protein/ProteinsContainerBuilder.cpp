5d51bc5f021de88ac437f6b3d922e85ec9078a97
Aurelien van Delft <avdelft@ulb.ac.be>
2016-03-26 21:36:39 +0100

    structure of ProteinsContainerBuilder

--- source/protein/ProteinsContainerBuilder.cpp
+++ source/protein/ProteinsContainerBuilder.cpp
@@ -1,1 +1,29 @@
 #include "ProteinsContainerBuilder.hpp"
+
+
+void ProteinsContainerBuilder::updateData(std::vector<std::string> proteins ,std::vector<std::string> partners, std::vector<std::string> constants)
+{
+	proteins_ = proteins;
+	partners_ = partners;
+	constants_ = constants;
+}
+
+void ProteinsContainerBuilder::buildGlobalProtein()
+{
+
+}
+
+void ProteinsContainerBuilder::buildProteinComplexes()
+{
+
+}
+
+void ProteinsContainerBuilder::buildProteinsContainer()
+{
+
+}
+
+ProteinsContainer* ProteinsContainerBuilder::getProteinsContainer() const
+{
+	return proteins_container_;
+}
--- source/protein/ProteinsContainerBuilder.hpp
+++ source/protein/ProteinsContainerBuilder.hpp
@@ -1,8 +1,30 @@
 #ifndef __PROTEINS_CONTAINER_BUILDER_HPP
 #define __PROTEINS_CONTAINER_BUILDER_HPP
 
+#include "ProteinsContainer.hpp"
+
 
 class ProteinsContainerBuilder {
+
+private:
+
+	ProteinsContainer* proteins_container_;
+	std::vector<GlobalProtein*> global_proteins_;
+	std::vector<ProteinComplex*> protein_complexes_;
+	std::vector<std::string> proteins_;
+	std::vector<std::string> partners_;
+	std::vector<std::string> constants_;
+
+public:
+
+	void updateData(std::vector<std::string> proteins ,std::vector<std::string> partners, std::vector<std::string> constants);
+	void buildGlobalProtein();
+	void buildProteinComplexes();
+	void buildProteinsContainer();
+	ProteinsContainer* getProteinsContainer() const;
+
+
+
 
 };
 
