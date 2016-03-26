ec4e4f3ab0756a458e22a8de6b93de5c998bc445
Aurelien van Delft <avdelft@ulb.ac.be>
2016-03-26 17:50:37 +0100

    changing container constructor

--- source/protein/ProteinsContainer.cpp
+++ source/protein/ProteinsContainer.cpp
@@ -1,3 +1,8 @@
 #include "ProteinsContainer.hpp"
 
-ProteinsContainer::ProteinsContainer(std::vector<GlobalProtein*> proteins): proteins_(proteins) {}
+ProteinsContainer::ProteinsContainer(std::vector<GlobalProtein*> proteins): proteins_(proteins)
+{
+	for (GlobalProtein* protein : proteins){
+		map_.insert(proteins_pair(protein, std::vector<ProteinComplex*>()));
+	}
+}
