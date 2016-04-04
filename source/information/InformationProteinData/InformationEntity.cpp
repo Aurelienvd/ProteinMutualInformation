69fadc20436fff55894e78645ea9e2b99aa12ec8
Aurelien van Delft <avdelft@ulb.ac.be>
2016-04-04 22:28:05 +0200

    Information Entity done

--- source/information/InformationProteinData/InformationEntity.cpp
+++ source/information/InformationProteinData/InformationEntity.cpp
@@ -1,1 +1,20 @@
 #include "InformationEntity.hpp"
+
+
+InformationEntity::InformationEntity(GlobalProtein* protein): entity_(protein), related_complexes_() {}
+
+
+GlobalProtein* InformationEntity::getEntity() const
+{
+	return entity_;
+}
+
+std::vector<ProteinComplex*> InformationEntity::getRelatedComplexes() const
+{
+	return related_complexes_;
+}
+
+void InformationEntity::addRelatedComplex(ProteinComplex* complex)
+{
+	related_complexes_.push_back(complex);
+}
--- source/information/InformationProteinData/InformationEntity.hpp
+++ source/information/InformationProteinData/InformationEntity.hpp
@@ -2,11 +2,31 @@
 #define __INFORMATION_ENTITY_HPP
 
 
+#include <vector>
+#include <string>
+
+#include "../../protein/GlobalProtein.hpp"
+#include "../../protein/ProteinComplex.hpp"
+
 /*
 *** This class represents an information entity based on the Shannon's communication model. It has been adapted to proteins communication.
 */
 
 class InformationEntity {
+
+	private:
+
+		GlobalProtein* entity_;
+		std::vector<ProteinComplex*> related_complexes_;		// All the complexes containing the entity.
+
+	public:
+
+		InformationEntity(GlobalProtein* protein);
+
+		GlobalProtein* getEntity() const;
+		std::vector<ProteinComplex*> getRelatedComplexes() const;
+
+		void addRelatedComplex(ProteinComplex* complex);
 
 };
 
