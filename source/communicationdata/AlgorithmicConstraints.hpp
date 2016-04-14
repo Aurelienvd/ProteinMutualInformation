ececaf22bcd51375c14ef1e8a47c15e60629a5ae
Aurelien van Delft <avdelft@ulb.ac.be>
2016-04-14 23:08:42 +0200

    and more doc

--- source/communicationdata/AlgorithmicConstraints.hpp
+++ source/communicationdata/AlgorithmicConstraints.hpp
@@ -1,3 +1,8 @@
+
+/**
+ * An AlgorithmicConstraints defines the constraints that the information algorithms will have to respect.
+ */
+
 #ifndef __ALGORITHMIC_CONSTRAINTS_HPP
 #define __ALGORITHMIC_CONSTRAINTS_HPP
 
@@ -16,10 +21,33 @@ class AlgorithmicConstraints: public Data {
 
 	private:
 
+		//-------------------------
+		// Private data members
+		//-------------------------
+		//
+		// input_
+		//		The constraints of the input
+		//
+		// channel_
+		//		The constraints of the channel
+		//
+		// output_
+		//		The constraints of the output
+		//
+		// type_
+		//		The type of the algorithm that will be executed
+
 		ProteinConstraints* input_;
 		ProteinConstraints* channel_;
 		ProteinConstraints* output_;
 		int type_;
+
+		//-------------------------
+		// Private member functions
+		//-------------------------
+		//
+		// setProteinConstraints
+		//		Set the constraints of a protein.
 
 		void setProteinConstraints(ProteinConstraints* attr, std::shared_ptr<Protein> protein_name, int init, int final, float step);
 
@@ -30,15 +58,104 @@ class AlgorithmicConstraints: public Data {
 		AlgorithmicConstraints();
 		virtual ~AlgorithmicConstraints();
 
+		//--------------------------
+		// Getters and Setters
+		//--------------------------
+
+		/**
+		 * Get the constraints of the input protein.
+		 *
+		 * @return A ProteinConstraints pointer that contains the constraints for the input.
+		 *
+		 * @see ProteinConstraints
+		 */
+
 		ProteinConstraints* getInput() const;
+
+		/**
+		 * Get the constraints of the output protein.
+		 *
+		 * @return A ProteinConstraints pointer that contains the constraints for the output.
+		 *
+		 * @see ProteinConstraints
+		 */
+
 		ProteinConstraints* getOutput() const;
+
+		/**
+		 * Get the constraints of the channel protein.
+		 *
+		 * @return A ProteinConstraints pointer that contains the constraints for the channel.
+		 *
+		 * @see ProteinConstraints
+		 */
+
 		ProteinConstraints* getChannel() const;
+
+		/**
+		 * Get the type of the algorithm that will be executed.
+		 *
+		 * @return A value defined in the enum.
+		 *
+		 * @see ProteinConstraints
+		 */
+
 		int getMutualInformationType() const;
 
+		/**
+		 * Set the constraints of the input protein.
+		 *
+		 * @param protein A std::shared_ptr to an instance of the Protein class
+		 * @param init The required starting value of the protein concentration.
+		 * @param final The required ending value of the protein concentration.
+		 * @param step The required increment of the protein concentration.
+		 *
+		 * @see ProteinConstraints
+		 */
+
 		void setInput(std::shared_ptr<Protein> protein, int init, int final, float step);
+
+		/**
+		 * Set the constraints of the output protein.
+		 *
+		 * @param protein A std::shared_ptr to an instance of the Protein class
+		 * @param init The required starting value of the protein concentration.
+		 * @param final The required ending value of the protein concentration.
+		 * @param step The required increment of the protein concentration.
+		 *
+		 * @see ProteinConstraints
+		 */
+
 		void setOutput(std::shared_ptr<Protein> protein, int init, int final, float step);
+
+		/**
+		 * Set the constraints of the channel protein.
+		 *
+		 * @param protein A std::shared_ptr to an instance of the Protein class
+		 * @param init The required starting value of the protein concentration.
+		 * @param final The required ending value of the protein concentration.
+		 * @param step The required increment of the protein concentration.
+		 *
+		 * @see ProteinConstraints
+		 */
+
 		void setChannel(std::shared_ptr<Protein> protein, int init, int final, float step);
+
+		/**
+		 * Set the type of the algorithm that will be executed.
+		 *
+		 * @param type The required type of the algorithm. The possible values are the ones defined in the enum.
+		 */
+
 		void setMutualInformationType(int type);
+
+		/**
+		 * ProteinConstraints defines algorithmic constraints for one protein.
+		 * It has to be noted that there are no setters nor constructors for this class.
+		 * The only way to initialise a ProteinConstraints is by using the AlgorithmicConstraints class.
+		 * Indeed, the AlgorithmicConstraints class is a friend of the ProteinConstraints class and as so,
+		 * it has access to all the private fields of the class.
+		 */
 
 		class ProteinConstraints {
 			private:
@@ -48,14 +165,42 @@ class AlgorithmicConstraints: public Data {
 				int final_value_;
 				float step_;
 
+				ProteinConstraints() = default;
+				
 				friend class AlgorithmicConstraints;
 
 			public:
-				ProteinConstraints() = default;
+
+				/**
+				 * Get the protein.
+				 *
+				 * @return A std::shared_ptr to a Protein instance.
+				 */
 
 				std::shared_ptr<Protein> getProtein() const {return protein_;}
+
+				/**
+				 * Get the starting value of the protein concentration.
+				 *
+				 * @return The starting value.
+				 */
+
 				int getInitialValue() const {return initial_value_;}
+
+				/**
+				 * Get the ending value of the protein concentration.
+				 *
+				 * @return The ending value.
+				 */
+
 				int getFinalValue() const {return final_value_;}
+
+				/**
+				 * Get the increment of the protein concentration.
+				 *
+				 * @return The increment value.
+				 */
+
 				float getStep() const {return step_;}
 
 		};
